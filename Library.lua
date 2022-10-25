a2b3dfdf442eec96be1925e63b380384b88c9215
Yuna <49483389+NullMaplee@users.noreply.github.com>
2022-10-25 19:35:12 -0400

    Update Library.lua
    Changed toggle key to correspond to the default setting toggle key

--- Library.lua
+++ Library.lua
@@ -885,7 +885,7 @@ function Library:create(options)
 	settingsTab:keybind{
 		Name = "Toggle Key",
 		Description = "Key to show/hide the UI.",
-		Keybind = Enum.KeyCode.Home,
+		Keybind = Library.ToggleKey,
 		Callback = function()
 			self.Toggled = not self.Toggled
 			Library:show(self.Toggled)
